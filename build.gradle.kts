ee1ec78c6f6385dcebbfa83dc2ca7b90548bda13
Qlllaa <qlllaa@users.noreply.github.com>
2026-08-16 16:45:39 +0800

    Fix build.gradle.kts and remove conflicting Groovy file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencies {
-    classpath("com.android.tools.build:gradle:8.5.2")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
